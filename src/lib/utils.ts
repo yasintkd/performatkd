5892f4e89c47e45ce2d72a147e6854fb72b862a0
yasintkd <yasinceken@gmail.com>
2026-08-04 23:45:39 +0300

    fix: tailwind-merge bağımlılığını kaldır, cn yardımcı fonksiyonunu bağımlılıksız yap

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,1 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...c: (string | undefined | null | false)[]) => c.filter(Boolean).join(' ')
